c5c9d5d728105e84ba72dd0c509d4c057a89401d
baptiste.crespy@gmail.com <baptiste.crespy@gmail.com>
2021-11-06 19:11:23 +0200

    📝 added comment about Features in the index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,20 @@ console.log(fxrand()) // deterministic PRNG function, use it instead of Math.ran
 // when the "fxhash" is always the same, it will generate the same sequence of
 // pseudo random numbers, always
 
+//----------------------
+// defining features
+//----------------------
+// You can define some token features by populating the $fxhashFeatures property
+// of the window object.
+// More about it in the guide, section features:
+// [https://fxhash.xyz/articles/guide-mint-generative-token#features]
+//
+// window.$fxhashFeatures = {
+//   "Background": "Black",
+//   "Number of lines": 10,
+//   "Inverted": true
+// }
+
 // this code writes the values to the DOM as an example
 const container = document.createElement("div")
 container.innerText = `
