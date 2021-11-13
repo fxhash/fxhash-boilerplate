71a7ad027f40155cfb0941faa1ec6741b34e187a
TengilLejonhjarta <tengillejonhjarta@gmail.com>
2021-11-13 15:43:41 +0100

    p5js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+import p5 from "p5";
+
 // these are the variables you can use as inputs to your algorithms
 console.log(fxhash)   // the 64 chars hex number fed to your algorithm
 console.log(fxrand()) // deterministic PRNG function, use it instead of Math.random()
-
+const seed = ~~(fxrand()*123456789);
+let s;
 // note about the fxrand() function 
 // when the "fxhash" is always the same, it will generate the same sequence of
 // pseudo random numbers, always
@@ -21,9 +24,33 @@ console.log(fxrand()) // deterministic PRNG function, use it instead of Math.ran
 // }
 
 // this code writes the values to the DOM as an example
-const container = document.createElement("div")
-container.innerText = `
-  random hash: ${fxhash}\n
-  some pseudo random values: [ ${fxrand()}, ${fxrand()}, ${fxrand()}, ${fxrand()}, ${fxrand()},... ]\n
-`
-document.body.prepend(container)
+
+let sketch = function(p5) {
+
+  p5.setup = function() {
+    p5.noLoop();
+    s = p5.min(p5.windowWidth, p5.windowHeight);
+    p5.createCanvas(s, s);
+  };
+
+  p5.draw = function() {
+    p5.random(seed);
+    p5.background("#FFD");
+    p5.push();
+    for (var i = 500; i >= 0; i--) {
+      let c = p5.color(p5.random(["#50B", "#313", "#713"]));
+      c.setAlpha(13);
+      p5.fill(c);
+      p5.noStroke();
+      p5.circle(p5.random()*p5.width, p5.random()*p5.height, p5.abs(p5.randomGaussian(0, p5.height/20)));
+    }
+    p5.pop();
+  };
+
+  p5.windowResized = function() {
+    s = p5.min(p5.windowWidth, p5.windowHeight);
+    p5.resizeCanvas(s, s);
+  }
+}
+
+let myp5 = new p5(sketch, window.document.body);
