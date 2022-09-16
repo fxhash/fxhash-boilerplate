decfbbda035265f9022ed1e3c900d56f96589a47
x0y0z0tn <111699853+x0y0z0tn@users.noreply.github.com>
2022-09-16 10:40:34 -0300

    a reminder related to add the license lines
    It will remind the developer to add the license lines.
    That is required by the code of conduct:
    This file should contain any relevant licensing information ...

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+/* @license add your license lines here.
+These lines will be add by webpack to bundle.js.LICENSE.txt
+*/
+
 // these are the variables you can use as inputs to your algorithms
 console.log(fxhash)   // the 64 chars hex number fed to your algorithm
 console.log(fxrand()) // deterministic PRNG function, use it instead of Math.random()
