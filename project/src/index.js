311e23c82e03e8b8cb2c1ae017016fc4e19a296a
maerzhase <markus@lili16.de>
2023-06-09 17:53:29 +0200

    adds param update modes
    available modes are the already known default mode "page-reload" and the
    new mode "sync" enables live syncing params without reloading the artwork
    also adds event subscription via $fx.on

--- project/src/index.js
+++ project/src/index.js
@@ -1,8 +1,8 @@
-console.log(fxhash)
-console.log(fxrand())
+// console.log(fxhash);
+// console.log(fxrand());
 
-const sp = new URLSearchParams(window.location.search);
-console.log(sp);
+const sp = new URLSearchParams(window.location.search)
+//  console.log(sp);
 
 // this is how to define parameters
 $fx.params([
@@ -11,13 +11,14 @@ $fx.params([
     name: "A number/float64",
     type: "number",
     //default: Math.PI,
+    update: "sync",
     options: {
       min: 1,
       max: 10,
-      step: 0.00000000000001,
+      step: 0.0001,
     },
   },
-  
+
   {
     id: "bigint_id",
     name: "A bigint",
@@ -36,8 +37,8 @@ $fx.params([
     //default: "hello",
     options: {
       minLength: 1,
-      maxLength: 512
-    }
+      maxLength: 512,
+    },
   },
   {
     id: "select_id",
@@ -46,12 +47,13 @@ $fx.params([
     //default: "pear",
     options: {
       options: ["apple", "orange", "pear"],
-    }
+    },
   },
   {
     id: "color_id",
     name: "A color",
     type: "color",
+    update: "sync",
     //default: "ff0000",
   },
   {
@@ -67,50 +69,73 @@ $fx.params([
     //default: "hello",
     options: {
       minLength: 1,
-      maxLength: 512
-    }
+      maxLength: 512,
+    },
   },
-]);
+])
 
 // this is how features can be defined
 $fx.features({
   "A random feature": Math.floor($fx.rand() * 10),
   "A random boolean": $fx.rand() > 0.5,
-  "A random string": ["A", "B", "C", "D"].at(Math.floor($fx.rand()*4)),
+  "A random string": ["A", "B", "C", "D"].at(Math.floor($fx.rand() * 4)),
   "Feature from params, its a number": $fx.getParam("number_id"),
 })
 
-// log the parameters, for debugging purposes, artists won't have to do that
-console.log("Current param values:")
-// Raw deserialize param values 
-console.log($fx.getRawParams())
-// Added addtional transformation to the parameter for easier usage
-// e.g. color.hex.rgba, color.obj.rgba.r, color.arr.rgb[0] 
-console.log($fx.getParams())
+function main() {
+  // log the parameters, for debugging purposes, artists won't have to do that
+  // console.log("Current param values:");
+  // // Raw deserialize param values
+  // console.log($fx.getRawParams());
+  // // Added addtional transformation to the parameter for easier usage
+  // // e.g. color.hex.rgba, color.obj.rgba.r, color.arr.rgb[0]
+  // console.log($fx.getParams());
 
-// how to read a single raw parameter
-console.log("Single raw value:")
-console.log($fx.getRawParam("color_id"));
-// how to read a single transformed parameter
-console.log("Single transformed value:")
-console.log($fx.getParam("color_id"));
+  // // how to read a single raw parameter
+  // console.log("Single raw value:");
+  // console.log($fx.getRawParam("color_id"));
+  // // how to read a single transformed parameter
+  // console.log("Single transformed value:");
+  // console.log($fx.getParam("color_id"));
 
-// update the document based on the parameters
-document.body.style.background = $fx.getParam("color_id").hex.rgba
-document.body.innerHTML = `
-<p>
-url: ${window.location.href}
-</p>
-<p>
-hash: ${$fx.hash}
-</p>
-<p>
-params:
-</p>
-<pre>
-${$fx.stringifyParams($fx.getRawParams())}
-</pre>
-<pre style="color: white;">
-${$fx.stringifyParams($fx.getRawParams())}
-</pre>
-`
+  const getContrastTextColor = (backgroundColor) =>
+    ((parseInt(backgroundColor, 16) >> 16) & 0xff) > 0xaa
+      ? "#000000"
+      : "#ffffff"
+
+  const bgcolor = $fx.getParam("color_id").hex.rgba
+  const textcolor = getContrastTextColor(bgcolor.replace("#", ""))
+
+  // update the document based on the parameters
+  document.body.style.background = bgcolor
+  document.body.innerHTML = `
+  <div style="color: ${textcolor};">
+    <p>
+    hash: ${$fx.hash}
+    </p>
+    <p>
+    minter: ${$fx.minter}
+    </p>
+    <p>
+    inputBytes: ${$fx.inputBytes}
+    </p>
+    <p>
+    params:
+    </p>
+    <pre>
+    ${$fx.stringifyParams($fx.getRawParams())}
+    </pre>
+  <div>
+  `
+}
+
+main()
+
+$fx.on(
+  "updateParams",
+  (newRawValues) => {
+    if (newRawValues.number_id.value === 5) return true
+    return false
+  },
+  () => main()
+)
