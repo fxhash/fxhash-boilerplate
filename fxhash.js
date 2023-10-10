9daa2f93e75c26a5282f9316cd705451f4f07ab4
crypt0biwan <crypt0biwan@gmail.com>
2023-10-10 10:32:20 +0200

    fixed "emit random parameters" button

--- fxhash.js
+++ fxhash.js
@@ -475,7 +475,7 @@
       },
       getRandomParam: function(id) {
         const definition = this._params.find((d) => d.id === id);
-        const processor = processors[definition.type];
+        const processor = ParameterProcessors[definition.type];
         return processor.random(definition);
       },
       getDefinitions: function() {
